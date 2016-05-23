d505970e06d42895ba8624a9da0d85899d056ef0
Gatunox <gastonzuccala@MacBook-Pro-Retina.local>
2016-05-23 19:25:55 -0300

    Initial Commit
    This is the initial commit of the basic splitter used to divide a HUGE
    content file into multiples lines

---
+++ splitter.py
@@ -0,0 +1,66 @@
+import sys, getopt, argparse, os.path
+
+def split():
+	sys.stdout.write("Starting script excution...\n\n")
+
+	fin = open(inputFile, 'r')
+	fout = open(outputFile, 'a')
+
+	linelegth = num(numberBytes)
+	totalbytes = 0
+	lines = 0
+
+	while True:
+		line = fin.read(linelegth)
+		if not line:
+		 	# sys.stdout.write("End of file\n")
+		 	break
+		totalbytes = totalbytes + linelegth
+		lines = lines + 1
+
+		fout.write(line)
+		fout.write("\n")
+
+	sys.stdout.write("Splitted %s bytes in %s lines \n\n" % (totalbytes,lines))
+	sys.stdout.write("End excution.")
+
+def verifyParameters(argv):
+
+	parser = argparse.ArgumentParser(description='Splitter script will split one file in multiple lines')
+	parser.add_argument('-i','--inputfile', dest="filename", metavar="FILE",  help='Input filename', required=True)
+	parser.add_argument('-b','--bytes', dest="bytes", help='Number of bytes per line', required=True)
+	
+	args = vars(parser.parse_args())
+
+	global inputFile
+	global outputFile
+	global numberBytes
+
+	inputFile = args['filename']
+	outputFile = args['filename'] + "-splitted"
+	numberBytes = args['bytes']
+
+	print ("Input file is ", inputFile)
+	print ("Output file is ", outputFile)
+	print ("Number of bytes ", numberBytes)
+
+def is_valid_file(file):
+    if not os.path.exists(file):
+        print ("The file %s does not exist!" % file)
+        sys.exit(2)
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        print ("The value %s is invalid!, Use a integer value" % s)
+        sys.exit(3)
+
+def main(argv):
+	sys.stdout.write("Python %s \n" % (sys.version))
+	verifyParameters(argv)
+	is_valid_file(inputFile)
+	split()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
